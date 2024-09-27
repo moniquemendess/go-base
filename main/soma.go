d9ebbf7a2ab5de6294ab5ccd9b75bb5d9896c48c
Monique Mendes <155989994+moniquemendess@users.noreply.github.com>
2024-09-27 15:29:27 +0200

    Main (#3)
    * Array Soma.go
    
    * Slice con TesTSoma
    
    * refaRefatoração

--- main/soma.go
+++ main/soma.go
@@ -1,11 +1,23 @@
 package main
 
 // O range permite que você percorra um array. Sempre que é chamado, retorna dois valores: o índice e o valor.
-func Soma(numeros [5]int) int {
+func Soma(numeros []int) int {
 	soma := 0
 
 	for _, numero := range numeros {
 		soma += numero
 	}
 	return soma
+ai
+
+// SomaTudo calcula as respectivas somas de cada slice recebido
+func SomaTodoOResto(numerosPararSomar ...[]int) []int {
+	var somas []int
+
+	for _, numeros := range numerosPararSomar {
+		somas = append(somas, Soma(numeros))
+	}
+
+	return somas
 }
+
--- main/soma_test.go
+++ main/soma_test.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+
+	"reflect"
 	"testing"
 )
 
 func TestSoma(t *testing.T) {
-	numeros := [5]int{1, 2, 3, 4, 5}
 
-	resultado := Soma(numeros)
-	esperado := 15
 
-	if esperado != resultado {
-		t.Errorf("resultado '%d', esperado '%d', dado '%v'", resultado, esperado, numeros)
+	t.Run("coleção de qualquer numero", func(t *testing.T) {
+		numeros := []int{1, 2, 3}
+
+		resultado := Soma(numeros)
+		esperado := 6
+
+		if resultado != esperado {
+			t.Errorf("resultado %d, esperado %d, dado, %v", resultado, esperado, numeros)
+		}
+	})
+
+}
+
+func TestSomaTodoOResto(t *testing.T) {
+
+	resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
+	esperado := []int{3, 9}
+
+	if !reflect.DeepEqual(resultado, esperado) {
+		t.Errorf("recebido %v esperado %v", resultado, esperado)
+
 	}
 }
