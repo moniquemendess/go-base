0b8787b5681866ce9e66c3371e44b6b76ec15ad1
Monique Mendes <moniiquemendes96@gmail.com>
2024-09-27 11:45:30 +0200

    Array Soma.go

---
+++ main/soma.go
@@ -0,0 +1,11 @@
+package main
+
+// O range permite que você percorra um array. Sempre que é chamado, retorna dois valores: o índice e o valor.
+func Soma(numeros [5]int) int {
+	soma := 0
+
+	for _, numero := range numeros {
+		soma += numero
+	}
+	return soma
+}
---
+++ main/soma_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSoma(t *testing.T) {
+	numeros := [5]int{1, 2, 3, 4, 5}
+
+	resultado := Soma(numeros)
+	esperado := 15
+
+	if esperado != resultado {
+		t.Errorf("resultado '%d', esperado '%d', dado '%v'", resultado, esperado, numeros)
+	}
+}
